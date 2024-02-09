840f7f8cb916ec9d4e82b6587051298f51881c89
shifinmalik <shifinmalik@gmail.com>
2024-02-09 08:11:24 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./Components/Navbar/NavBar";
+import "./App.css"
+import {action,originals,trending,Comedy,Horror,Romance,Document} from './urls'
+import Banner from "./Components/Banner/Banner";
+import RowPost from "./Components/RowPost/RowPost";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+      <Banner/>
+      
+      <RowPost url={originals} title='NetFlix Originals'/>
+      <RowPost url={trending} title='Trending'/>
+      <RowPost  url={action} title='Actions' isSmall/>
+      <RowPost  url={Comedy} title='Comedy' isSmall/>
+      <RowPost  url={Horror} title='Horror' isSmall/>
+      <RowPost  url={Romance} title='Romance' isSmall/>
+      <RowPost  url={Document} title='Document' isSmall/>
+
     </div>
   );
 }
---
+++ src/constants/constants.js
@@ -0,0 +1,8 @@
+
+
+
+export const baseUrl ='https://api.themoviedb.org/3';
+
+export const API_KEY ="dd849598f23eb6a591f719bcd679aa6a";
+
+export const imageUrl='https://image.tmdb.org/t/p/original';
